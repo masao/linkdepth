42d39394f2898d4018fc8973ef934fddcacb4879
masao <tmasao@acm.org>
2009-02-22 02:38:06 +0000

    SE, SR$B$r<4L\@9$j>e$KI=5-$9$k$h$&DI2C(B

--- make_gnuplot.sh
+++ make_gnuplot.sh
@@ -8,10 +8,13 @@ for f in "$@"; do
     ./ext-search.rb $f $basename > search
     echo "set term png size 800,800" >> search
     echo "set size square" >> search
-    echo "set xrange [-6:13.5]" >> search
+    echo "set xrange [-6:19.5]" >> search
     echo "set yrange [0:1200] reverse" >> search
     echo "set zeroaxis" >> search
-    echo "set xtics 0,1" >> search
+    #echo "set xtics 0,1" >> search
+    echo "set xtics ('SE' -1, 'SR' 0)" >> search
+    echo "set xtics ('SE' -1, 'SR' 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)" >> search
+    echo "set x2tics ('SE' -1, 'SR' 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)" >> search
     echo "set ytics 60" >> search
     echo "plot \
         \"${basename}.txt\" using 2:1 title \"$basename\" w linespoint, \
